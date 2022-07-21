e7e87248f730fc607a95bba68544e47849d5900c
Buckley Weglarz <buckley.weglarz@gmail.com>
2022-07-20 18:30:13 -0700

    Add files via upload

--- fullstack-app/populate_db.py
+++ fullstack-app/populate_db.py
@@ -1,9 +1,16 @@
 import sqlite3
+import alpaca_trade_api as tradeapi
 
 connection = sqlite3.connect('app.db')
 
 cursor = connection.cursor()
 
-cursor.execute("DELETE FROM stock")
+api = tradeapi.REST('AKORBK3PUHT6IVABBPQL', 'nRnMWK24fAfNERXbANKVncWddzzweWARqqZ3innA', base_url='https://api.alpaca.markets')
+assets = api.list_assets()
+
+for asset in assets:
+    print(asset)
+
+#cursor.execute("DELETE FROM stock")
 
 connection.commit()
